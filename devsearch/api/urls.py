8598a959e28f7e0e49a4dd2f70849531defbeaee
elizabeth <63110747+kttel@users.noreply.github.com>
2022-12-14 06:50:51 +0200

    Updated project files

--- devsearch/api/urls.py
+++ devsearch/api/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from . import views
 
@@ -7,4 +11,8 @@ urlpatterns = [
     path('', views.getRoutes),
     path('projects/', views.getProjects),
     path('projects/<str:pk>/', views.getProject),
+    path('projects/<str:pk>/vote/', views.projectVote),
+
+    path('users/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
